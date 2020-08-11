8ce87df9982520f734bb95a602c1eb2e22e51b46
fffibonacci <yifanchen@intl.zju.edu.cn>
2020-08-11 20:39:05 +0800

    gradient“

--- define_model.py
+++ define_model.py
@@ -106,11 +106,11 @@ def define_model(Data, opt):
     np.save(opt.result_path + '/gt_labels.npy', Data['test'][1])
     
     model.fit_generator(
-            # no_aug_generator(Data['train'][0], Data['train'][1],batch_size=bs),
-            custom_image_generator(Data['train'][0], Data['train'][1], bs),
+            no_aug_generator(Data['train'][0], Data['train'][1],batch_size=bs),
+            # custom_image_generator(Data['train'][0], Data['train'][1], bs),
             steps_per_epoch= n_train//bs, epochs=n_epoch, verbose=1,
-            #validation_data=(Data['val'][0], Data['val'][1]),
-            validation_data=val_datagenerator(Data['val'][0], Data['val'][1], use_gradient),  # no gen
+            validation_data=(Data['val'][0], Data['val'][1]),
+            # validation_data=val_datagenerator(Data['val'][0], Data['val'][1], use_gradient),  # no gen
             validation_steps= n_val,
             callbacks=callbacks)
     
